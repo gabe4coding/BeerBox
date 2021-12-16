7397b0eefdf6904f072a3ef40ce1309e1b9c63f0
Gabriele Pavanello <gabriele.pavanello@accenture.com>
2021-12-16 13:44:13 +0100

    Initial Commit

---
+++ BeerBox/BeerBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerBoxApp.swift
+//  BeerBox
+//
+//  Created by Gabriele Pavanello on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BeerBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
